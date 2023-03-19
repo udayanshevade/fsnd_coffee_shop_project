b10180e2d4a3ef9e3846594467a2ac650f2f58d3
Udayan Shevade <udayan.shevade@gmail.com>
2023-03-18 19:38:29 -0700

    add drink delete endpoint

--- backend/src/api.py
+++ backend/src/api.py
@@ -72,7 +72,6 @@ def get_detailed_drinks():
 
 
 '''
-@TODO implement endpoint
     POST /drinks
         it should create a new row in the drinks table
         it should require the 'post:drinks' permission
@@ -106,7 +105,6 @@ def create_drink():
 
 
 '''
-@TODO implement endpoint
     PATCH /drinks/<id>
         where <id> is the existing model id
         it should respond with a 404 error if <id> is not found
@@ -138,7 +136,7 @@ def update_drink(drink_id):
         return jsonify({
             'success': True,
             'drinks': drink
-        })
+        }), 200
     except Exception as e:
         print('Error - [PATCH] /drinks/<id>')
         code = getattr(e, 'code', 500)
@@ -148,7 +146,6 @@ def update_drink(drink_id):
 
 
 '''
-@TODO implement endpoint
     DELETE /drinks/<id>
         where <id> is the existing model id
         it should respond with a 404 error if <id> is not found
@@ -157,6 +154,25 @@ def update_drink(drink_id):
     returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
         or appropriate status code indicating reason for failure
 '''
+
+
+@requires_auth('delete:drinks')
+@app.route('/drinks/<int:drink_id>', methods=['DELETE'])
+def delete_drink(drink_id):
+    try:
+        print('Request - [PATCH] /drinks/<id>')
+        drink = Drink.query.get(drink_id)
+        drink.delete()
+        return jsonify({
+            'success': True,
+            'delete': drink_id
+        }), 200
+    except Exception as e:
+        print('Error - [PATCH] /drinks/<id>')
+        code = getattr(e, 'code', 500)
+        abort(code)
+    finally:
+        db.session.close()
 
 
 # Error Handling
