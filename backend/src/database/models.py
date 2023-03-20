82f77c9d987fd3abf37d653b4f83dc839315430f
Udayan Shevade <udayan.shevade@gmail.com>
2023-03-19 20:49:30 -0700

    update postman collection jwts, cleanup

--- backend/src/database/models.py
+++ backend/src/database/models.py
@@ -66,7 +66,6 @@ class Drink(db.Model):
     '''
 
     def short(self):
-        print(json.loads(self.recipe))
         short_recipe = [{'color': r['color'], 'parts': r['parts']}
                         for r in json.loads(self.recipe)]
         return {
